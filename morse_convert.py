d9b53f56b20021d7778c9ef1df239273a32cbaa7
blockynight <joncpim@gmail.com>
2012-12-30 18:41:25 +0000

    Finished sound creation and playback of converted message.

--- morse_convert.py
+++ morse_convert.py
@@ -1,6 +1,8 @@
-# A program to convert strings to morse code.
+# A program to convert strings to their morse code equivilent 
+# and create and play a .wav file of that morse code message.
 
 import os
+import wave
 
 def main():
     
@@ -16,16 +18,50 @@ def main():
     conv_string = ""
 
     for letter in string:
-        conv_string += morse_table[letter]
 
-    print conv_string
+        if letter in morse_table:
+            conv_string += morse_table[letter]  
 
-    for l in conv_string:
-        if '_' == l:
-            os.system("aplay morse_long.wav")
-        elif '.' == l:
-            os.system("aplay morse_short.wav")
         else:
-            pass
+            print "\nWARNING: '%s' has no morse code equivilent." % letter
+            print "\t A pause will be added to the .wav at this character's position."
+            print "\t Use only the 26 letters (a -> z) and 10 numbers (0 -> 9)\n"
+            conv_string += morse_table[' ']
+
+    print "Morse Code> " + conv_string
+
+    sound(conv_string)
+
+
+def sound(morse_string):   # Creates and plays .wav of your message in morse code.
+
+    long_sound = wave.open("morse_long.wav", 'r')
+    short_sound = wave.open("morse_short.wav", 'r')
+    silent = wave.open("morse_pause.wav", 'r')
+    full_sound = wave.open("full_morse.wav", 'w')
+
+    full_sound.setparams(long_sound.getparams())
+
+    for char in morse_string:
+
+        if '_' == char:
+            full_sound.writeframes(long_sound.readframes(long_sound.getnframes()))
+            long_sound.rewind()
+
+        elif '.' == char:
+            full_sound.writeframes(short_sound.readframes(short_sound.getnframes()))
+            short_sound.rewind()
+
+        else:
+            full_sound.writeframes(silent.readframes(silent.getnframes()))
+            silent.rewind()
+
+    long_sound.close()
+    short_sound.close()
+    silent.close()
+    full_sound.close()
+
+    os.system("aplay full_morse.wav")
+
 
 main()
